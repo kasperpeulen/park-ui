9d6685569b76c8e5bce86ce32ba7bed2046137d3
Kasper Peulen <kasperpeulen@gmail.com>
2026-01-16 12:27:28 +0100

    qa: enable react-docgen-typescript for QA2 story creation test

--- components/react/.storybook/main.ts
+++ components/react/.storybook/main.ts
@@ -1,21 +1,21 @@
-import { fileURLToPath } from "node:url";
-import { dirname } from "node:path";
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  framework: getAbsolutePath("@storybook/react-vite"),
+  framework: getAbsolutePath('@storybook/react-vite'),
   stories: ['../src/examples/**/*.stories.tsx'],
-  addons: [getAbsolutePath("@storybook/addon-a11y"), getAbsolutePath("@storybook/addon-themes")],
+  addons: [getAbsolutePath('@storybook/addon-a11y'), getAbsolutePath('@storybook/addon-themes')],
   core: {
     disableTelemetry: true,
   },
   typescript: {
-    reactDocgen: false,
+    reactDocgen: 'react-docgen-typescript',
   },
 }
 
 export default config
 
 function getAbsolutePath(value: string): any {
-  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
 }
